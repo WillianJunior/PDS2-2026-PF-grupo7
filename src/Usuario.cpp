4ec9a33a9c07aad7ef635f5b9319b0a086ce713c
danielshuvarz <danielshuvarz@ufmg.br>
2026-06-09 01:34:46 -0300

    implmnt usuario

--- src/Usuario.cpp
+++ src/Usuario.cpp
@@ -1,19 +1,34 @@
 #include "Usuario.hpp"
 
-Usuario::Usuario(int id, const std::string& nome, const std::string& email, const std::string& senha) {}
+Usuario::Usuario(int id, const std::string& nome, const std::string& email, const std::string& senha)
+ :id(id),nome(nome),email(email), senha(senha) //lista de inicialização 
+ {
+}
 
 Usuario::Usuario() {}
 
-int Usuario::getId() const {return 0; }
+int Usuario::getId() const {
+    return this->id;
+}
+std::string Usuario::getNome() const {
+    this->nome;
+}
 
-std::string Usuario::getNome() const {return {}; }
+std::string Usuario::getEmail() const {
+   this->email;
+}
 
-std::string Usuario::getEmail() const {return {}; }
+std::string Usuario::getSenha() const {
+    return this-> senha; 
+}
 
-std::string Usuario::getSenha() const {return {}; }
+void Usuario::setNome(const std::string& nome) {
+    this-> nome = nome;
+}
+void Usuario::setEmail(const std::string& email) {
+    this -> email = email;
+}
 
-void Usuario::setNome(const std::string& nome) {}
-
-void Usuario::setEmail(const std::string& email) {}
-
-void Usuario::setSenha(const std::string& senha) {}
+void Usuario::setSenha(const std::string& senha) {
+    this->senha = senha;
+}
